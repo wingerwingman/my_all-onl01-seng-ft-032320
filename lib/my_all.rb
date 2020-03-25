9fca2dfe112e50512f4f3d1f264fdf4f8f3bf971
Justin Winger <justinwinger4@gmail.com>
2020-03-25 19:37:55 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -9,10 +9,9 @@ def my_all?(collection)
     i = i + 1
   end
  
-  if block_return_values.none?(false)
+  if block_return_values.any?(false)
     false
   else
     true
   end
-  block_return_values
 end
